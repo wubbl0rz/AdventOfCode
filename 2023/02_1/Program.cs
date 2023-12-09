14fce55803dd874e996efb8dde0e5201a0c9164a
wubbl0rz <wubbl0rz@gmx.de>
2023-12-09 15:24:31 +0100

    day02

---
+++ 2023/02_1/Program.cs
@@ -0,0 +1,52 @@
+﻿using System.Text.RegularExpressions;
+
+var input = File.ReadAllLines("input.txt");
+
+var limit = new GameLimit(12, 13, 14);
+
+var result = input
+  .Select(Game.Parse)
+  .Where(game => game.IsValidGame(limit))
+  .Sum(game => game.Id);
+
+Console.WriteLine(result);
+
+public record GameLimit(int LimitRed, int LimitGreen, int LimitBlue);
+
+public class Game
+{
+  public int Id { get; set; }
+  
+  public int MaxBlue { get; set; }
+  public int MaxRed { get; set; }
+  public int MaxGreen { get; set; }
+
+  public bool IsValidGame(GameLimit limit) => this.MaxRed <= limit.LimitRed && 
+                                              this.MaxGreen <= limit.LimitGreen && 
+                                              this.MaxBlue <= limit.LimitBlue;
+
+  public static Game Parse(string line)
+  {
+    var regex = new Regex(@"(^Game (\d+): |(\d+)\s(red|green|blue))");
+    
+    var matches = regex.Matches(line).ToArray();
+
+    var id = int.Parse(matches[0].Groups[2].ToString());
+
+    var result = matches[1..].Select(m => new
+      {
+        color = m.Groups[4].ToString(),
+        count = int.Parse(m.Groups[3].ToString())
+      })
+      .GroupBy(c => c.color)
+      .ToDictionary(g => g.Key, g => g.Max(c => c.count));
+
+    return new Game()
+    {
+      Id = id,
+      MaxBlue = result["blue"],
+      MaxRed = result["red"],
+      MaxGreen = result["green"],
+    };
+  }
+}
---
+++ 2023/02_2/Program.cs
@@ -0,0 +1,45 @@
+﻿using System.Text.RegularExpressions;
+using System.Threading.Channels;
+
+var input = File.ReadAllLines("input.txt");
+
+var result = input
+  .Select(Game.Parse)
+  .Select(game => game.MaxRed * game.MaxBlue * game.MaxGreen)
+  .Sum();
+
+Console.WriteLine(result);
+
+public class Game
+{
+  public int Id { get; set; }
+  
+  public int MaxBlue { get; set; }
+  public int MaxRed { get; set; }
+  public int MaxGreen { get; set; }
+
+  public static Game Parse(string line)
+  {
+    var regex = new Regex(@"(^Game (\d+): |(\d+)\s(red|green|blue))");
+    
+    var matches = regex.Matches(line).ToArray();
+
+    var id = int.Parse(matches[0].Groups[2].ToString());
+
+    var result = matches[1..].Select(m => new
+      {
+        color = m.Groups[4].ToString(),
+        count = int.Parse(m.Groups[3].ToString())
+      })
+      .GroupBy(c => c.color)
+      .ToDictionary(g => g.Key, g => g.Max(c => c.count));
+
+    return new Game()
+    {
+      Id = id,
+      MaxBlue = result["blue"],
+      MaxRed = result["red"],
+      MaxGreen = result["green"],
+    };
+  }
+}
