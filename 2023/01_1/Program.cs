9a76eb7a949fde657dacb2c5245316bb32c6bea0
wubbl0rz <wubbl0rz@gmx.de>
2023-12-09 13:45:47 +0100

    day01

---
+++ 2023/01_1/Program.cs
@@ -0,0 +1,11 @@
+﻿var input = File.ReadAllLines("input.txt");
+
+var result = input.Select(line =>
+{
+  var first = line.First(char.IsDigit);
+  var last = line.Last(char.IsDigit);
+
+  return int.Parse($"{first}{last}");
+}).Sum();
+
+Console.WriteLine(result);
---
+++ 2023/01_2/Program.cs
@@ -0,0 +1,39 @@
+﻿using System.Text.RegularExpressions;
+
+var input = File.ReadAllLines("input.txt");
+
+var digits = new Dictionary<string, string>()
+{
+  { "1", "1" },
+  { "2", "2" },
+  { "3", "3" },
+  { "4", "4" },
+  { "5", "5" },
+  { "6", "6" },
+  { "7", "7" },
+  { "8", "8" },
+  { "9", "9" },
+  { "one", "1" },
+  { "two", "2" },
+  { "three", "3" },
+  { "four", "4" },
+  { "five", "5" },
+  { "six", "6" },
+  { "seven", "7" },
+  { "eight", "8" },
+  { "nine", "9" },
+};
+
+var regex = new Regex($"(?=({string.Join("|", digits.Keys)})){{1}}");
+
+var result = input.Select(line =>
+{
+  var matches = regex.Matches(line);
+
+  var first = matches.First().Groups[1].Value;
+  var last = matches.Last().Groups[1].Value;
+
+  return int.Parse($"{digits[first]}{digits[last]}");
+}).Sum();
+
+Console.WriteLine(result);
